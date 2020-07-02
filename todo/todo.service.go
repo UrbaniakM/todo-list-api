e70d97809a3b249ae345544e9b0ce93782a93cbf
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-07-02 19:10:22 +0200

    Move todo handlers to separate file

---
+++ todo/todo.service.go
@@ -0,0 +1,128 @@
+package todo
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+	"strconv"
+	"time"
+)
+
+const todosBasePath = "todos"
+func SetupRoutes(apiBasePath string) {
+	todosHandlerFunc := http.HandlerFunc(todosHandler)
+	todoHandlerFunc := http.HandlerFunc(todoHandler)
+
+	http.Handle(fmt.Sprintf("%s/%s", apiBasePath, todosBasePath), middlewareHandler(todosHandlerFunc))
+	http.Handle(fmt.Sprintf("%s/%s/", apiBasePath, todosBasePath), middlewareHandler(todoHandlerFunc))
+}
+
+func middlewareHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		fmt.Println("before handler; middleware  start")
+		start := time.Now()
+		handler.ServeHTTP(w, r)
+		fmt.Printf("middleware  finished, %s", time.Since(start))
+	})
+}
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// get all todos
+		todoList := getTodoList()
+		todosJSON, err := json.Marshal(todoList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(todosJSON)
+		return
+	case http.MethodPost:
+		// add new todo
+		var newTodo Todo
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = json.Unmarshal(bodyBytes, &newTodo)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if newTodo.TodoId != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		_, err = addOrUpdateTodo(newTodo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		return 
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func todoHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "todos/")
+
+	todoId, err := strconv.Atoi(urlPathSegments[len(urlPathSegments) - 1])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return 
+	}
+
+	todo := getTodo(todoId)
+	if todo == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return 
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		// get single todo
+		todoJSON, err := json.Marshal(todo)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(todoJSON)
+		return
+	case http.MethodPut:
+		// update todo in the list
+		var updatedTodo Todo
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = json.Unmarshal(bodyBytes, &updatedTodo)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if updatedTodo.TodoId != todoId {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		addOrUpdateTodo(updatedTodo)
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
